100e00863a19ff0fb2d5534f4fbbcdadd7dd3e80
borbalakovacs <borbala.kovacs2@stud.uni-corvinus.hu>
2020-09-26 18:21:54 +0200

    c

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace UserMaintenance
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
+            button2.Text = Resource1.Write;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,6 +33,36 @@ namespace UserMaintenance
             user.LastName = textBox1.Text;
             user.FirstName = textBox2.Text;
             users.Add(user);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "users.txt";
+
+            sfd.FileOk += Sfd_FileOk;
+            sfd.ShowDialog();
+        }
+
+        private void Sfd_FileOk(object sender, CancelEventArgs e)
+
+        {
+
+            string docPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            using (StreamWriter sw = new StreamWriter(Path.Combine(docPath, "users.txt")))
+            {
+                foreach (var u in users)
+                {
+                    string str = u.ID + " ; " + u.FullName;
+                    sw.WriteLine(str);
+
+                }
+                sw.Close();
+
+            }
+
         }
     }
 }
