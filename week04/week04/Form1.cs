cf4c847125ff59bae4ff878b9ee0ad8e3bd00dc5
borbalakovacs <borbala.kovacs2@stud.uni-corvinus.hu>
2020-10-01 08:22:56 +0200

    commit

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.Reflection;
 
 namespace week04
 {
     public partial class Form1 : Form
     {
-        RealEstateEntities1 context = new RealEstateEntities1();
-        List<Flat> Flats;
-        Excel.Application xlApp;
-        Excel.Workbook xlWB;
-        Excel.Worksheet xlSheet;
+        private RealEstateEntities1 context = new RealEstateEntities1();
+        private List<Flat> Flats;
+        private Excel.Application xlApp;
+        private Excel.Workbook xlWB;
+        private Excel.Worksheet xlSheet;
 
         public Form1()
         {
@@ -31,9 +27,9 @@ namespace week04
         {
             Flats = context.Flats.ToList();
         }
+
         private void CreateExcel()
         {
-
             try
             {
                 // Excel elindítása és az applikáció objektum betöltése
@@ -67,17 +63,43 @@ namespace week04
 
         private void CreateTable()
         {
-
             string[] headers = new string[] {
-     "Kód",
-     "Eladó",
-     "Oldal",
-     "Kerület",
-     "Lift",
-     "Szobák száma",
-     "Alapterület (m2)",
-     "Ár (mFt)",
-     "Négyzetméter ár (Ft/m2)"};
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"};
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[i + 1, 1] = headers[i];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (var f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                if (f.Elevator == true)
+                {
+                    values[counter, 4] = "Van";
+                }
+                else
+                {
+                    values[counter, 4] = "Nincs";
+                }
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+            }
         }
     }
 }
