74398b24d6d51b94118a3b00e57efb5089133142
borbalakovacs <borbala.kovacs2@stud.uni-corvinus.hu>
2020-10-01 15:06:19 +0200

    commit

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -99,7 +99,29 @@ namespace week04
                 values[counter, 6] = f.FloorArea;
                 values[counter, 7] = f.Price;
                 values[counter, 8] = "";
+                counter++;
             }
+            xlSheet.get_Range(
+                GetCell(2, 1),
+                GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+
         }
     }
 }
