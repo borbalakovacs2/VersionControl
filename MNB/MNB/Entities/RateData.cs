79344ba72072372b8e3f8a82f9084cdc0f271fb6
borbalakovacs2 <borbala.kovacs2@stud.uni-corvinus.hu>
2020-10-17 14:17:00 +0200

    commit

---
+++ MNB/MNB/Entities/RateData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNB.Entities
+{
+    
+    class RateData
+    {
+        public DateTime Date;
+        public string Currency;
+        public decimal Value;
+
+    }
+}
