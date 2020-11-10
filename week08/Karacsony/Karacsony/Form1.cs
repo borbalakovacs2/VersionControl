7f2bb45207a4e27c2e8d0a229fc3dd558fbb307e
borbalakovacs2 <borbala.kovacs2@stud.uni-corvinus.hu>
2020-11-10 10:11:13 +0100

    commit

--- week08/Karacsony/Karacsony/Form1.cs
+++ week08/Karacsony/Karacsony/Form1.cs
@@ -30,11 +30,31 @@ namespace Karacsony
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = 0 - ball.Width;
+            mainPanel.Controls.Add(ball);
 
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
+            var position = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > position)
+                {
+                    position = ball.Left;
+                }
+                if (position > 1000)
+                {
+                    var first = _balls[0];
+                    mainPanel.Controls.Remove(first);
+                    _balls.Remove(first);
+
+                }
+            }
 
         }
     }
