427115398fd7571c73c5a7dee6b1189769fc80ad
borbalakovacs2 <borbala.kovacs2@stud.uni-corvinus.hu>
2020-11-10 09:56:24 +0100

    commit

---
+++ week08/Karacsony/Karacsony/Entities/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karacsony.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
