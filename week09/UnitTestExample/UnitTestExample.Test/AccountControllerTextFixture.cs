ecf2019d8cfa0b72ccfd7d127c3283be8123f787
borbalakovacs2 <borbala.kovacs2@stud.uni-corvinus.hu>
2020-11-19 18:45:39 +0100

    commit

---
+++ week09/UnitTestExample/UnitTestExample.Test/AccountControllerTextFixture.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTextFixture
+    {
+
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)]
+        public void TestValidate(string email, bool expectedValue)
+        {
+
+
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
+
