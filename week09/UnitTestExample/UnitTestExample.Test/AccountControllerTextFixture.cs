a91e4f2a2002275d34dfef220d1a23cd727e9afa
borbalakovacs2 <borbala.kovacs2@stud.uni-corvinus.hu>
2020-11-19 19:03:18 +0100

    commit

--- week09/UnitTestExample/UnitTestExample.Test/AccountControllerTextFixture.cs
+++ week09/UnitTestExample/UnitTestExample.Test/AccountControllerTextFixture.cs
@@ -29,6 +29,43 @@ namespace UnitTestExample.Test
             Assert.AreEqual(expectedResult, actualResult);
 
         }
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("AbCd1234", true),
+        TestCase("abCD@1234", true),
+        TestCase("irf", false),
+        TestCase("abcdabcd", false)]
+        public void PasswordValidate(string password, bool expectedResult)
+        {
+
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [
+        Test,
+        TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+        TestCase("irf@uni-corvinus.hu", "Abcd1234567"),]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
     }
 }
 
